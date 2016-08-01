a88f1150811046f62ce73c874c5f2085410e6499
Damien Jiang <damieneskimo@gmail.com>
2016-08-01 23:36:10 +1200

    delete simple theme

--- mysite/_config.php
+++ mysite/_config.php
@@ -6,7 +6,20 @@ $project = 'mysite';
 global $database;
 $database = '';
 
-require_once('conf/ConfigureFromEnv.php');
+//Heroku ClearDB support
+if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
+	global $databaseConfig;
+	$parts = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+	$databaseConfig['type'] = 'MySQLDatabase';
+	$databaseConfig['server'] = $parts['host'];
+	$databaseConfig['username'] = $parts['user'];
+	$databaseConfig['password'] = $parts['pass'];
+	$databaseConfig['database'] = trim($parts['path'], '/');
+	Security::setDefaultAdmin('heroku', 'yesletmeinplease');
+} else {
+	//Default SilverStripe environement support
+	require_once('conf/ConfigureFromEnv.php');
+}
 
 // Set the site locale
 i18n::set_locale('en_US');
